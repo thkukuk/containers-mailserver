2bab995d9a0f573a458cbc0810583b87dd610cbb
Thorsten Kukuk <kukuk@suse.com>
2020-08-15 12:13:47 +0200

    Restore sysconfig support, check for UID

--- openldap/entrypoint.sh
+++ openldap/entrypoint.sh
@@ -28,17 +28,70 @@ setup_timezone() {
 init_ldap_url() {
     test -n "${LDAP_URL}" && return
 
-    local FQDN
-    FQDN="$(/bin/hostname -f)"
-    LDAP_URL="lda://$FQDN:$LDAP_PORT"
+
+    if [ -n "${OPENLDAP_START_LDAP}" ]; then
+	case "$OPENLDAP_START_LDAP" in
+            [Yy][Ee][Ss])
+		if [ -n "$OPENLDAP_LDAP_INTERFACES" ]
+		then
+                    for iface in $OPENLDAP_LDAP_INTERFACES ;do
+			LDAP_URL="$LDAP_URL ldap://$iface"
+                    done
+		else
+                    LDAP_URL="ldap:///"
+		fi
+		;;
+	esac
+    else
+	local FQDN
+	FQDN="$(/bin/hostname -f)"
+	LDAP_URL="ldap://$FQDN:$LDAP_PORT"
+    fi
 }
 
 init_ldaps_url() {
     test -n "${LDAPS_URL}" && return
 
-    local FQDN
-    FQDN="$(/bin/hostname -f)"
-    LDAPS_URL="ldaps://$FQDN:$LDAPS_PORT"
+    if [ -n "${OPENLDAP_START_LDAPS}" ]; then
+	case "$OPENLDAP_START_LDAPS" in
+            [Yy][Ee][Ss])
+		if [ -n "$OPENLDAP_LDAP_INTERFACES" ]
+		then
+                    for iface in $OPENLDAP_LDAPS_INTERFACES ;do
+			LDAPS_URL="$LDAPS_URL ldaps://$iface"
+                    done
+		else
+                    LDAPS_URL="ldaps:///"
+		fi
+		;;
+	esac
+    else
+	local FQDN
+	FQDN="$(/bin/hostname -f)"
+	LDAPS_URL="ldaps://$FQDN:$LDAPS_PORT"
+    fi
+}
+
+setup_ldap_uidgid() {
+    CUR_LDAP_UID=$(id -u ldap)
+    CUR_LDAP_GID=$(id -g ldap)
+
+    LDAP_UIDGID_CHANGED=false
+    if [ "$LDAP_UID" != "$CUR_LDAP_UID" ]; then
+	echo "Current ldap UID (${CUR_LDAP_UID}) does not match LDAP_UID (${LDAP_UID}), adjusting..."
+	usermod -o -u "$LDAP_UID" ldap
+	LDAP_UIDGID_CHANGED=true
+    fi
+    if [ "$LDAP_GID" != "$CUR_USER_GID" ]; then
+	echo "Current ldap GID (${CUR_LDAP_GID}) does't match LDAP_GID (${LDAP_GID}), adjusting..."
+	groupmod -o -g "$LDAP_GID" ldap
+	LDAP_UIDGID_CHANGED=true
+    fi
+
+    echo 'OpenLDAP GID/UID'
+    echo "User uid:    $(id -u openldap)"
+    echo "User gid:    $(id -g openldap)"
+    echo "uid/gid changed: ${LDAP_UIDGID_CHANGED}"
 }
 
 # usage: file_env VAR [DEFAULT]
@@ -68,11 +121,14 @@ if [ "${1:0:1}" = '-' ]; then
         set -- /usr/sbin/slapd "$@"
 fi
 
+test -f /etc/sysconfig/openldap && . /etc/sysconfig/openldap
+
 # Reduce maximum number of number of open file descriptors
 # see https://github.com/docker/docker/issues/8231
 ulimit -n "$LDAP_NOFILE"
 
 setup_timezone
+setup_ldap_uidgid
 init_ldap_url
 init_ldaps_url
 
