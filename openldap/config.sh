ada5eebff8bebc46907b64e233cd129776e874e4
Thorsten Kukuk <kukuk@suse.com>
2020-08-15 12:13:34 +0200

    Remove sysconfig/openldap away

---
+++ openldap/config.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+  
+#======================================
+# Functions...
+#--------------------------------------
+test -f /.profile && . /.profile
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+echo "Move /etc/sysconfig/openldap away"
+
+mv /etc/sysconfig/openldap /etc/sysconfig/openldap.example
