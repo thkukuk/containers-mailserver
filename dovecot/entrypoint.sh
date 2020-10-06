5dc7a51cbd604c6531532c78943c54e5dcdf0d8c
Thorsten Kukuk <kukuk@suse.com>
2020-10-06 08:11:20 +0200

    Add setup functions for LDAP

--- dovecot/entrypoint.sh
+++ dovecot/entrypoint.sh
@@ -4,17 +4,17 @@
 
 export PATH=/usr/sbin:/sbin:${PATH}
 
-DOVECOT_RUN_DIR=${SLAPD_RUN_DIR:-"/run/dovecot"}
+DOVECOT_RUN_DIR=${DOVECOT_RUN_DIR:-"/run/dovecot"}
 
 # Default values for new database
-DOVECOT_ORGANISATION=${DOVECOT_ORGANISATION:-"Example Inc."}
+DOVECOT_ORGANIZATION=${DOVECOT_ORGANIZATION:-"Example Inc."}
 DOVECOT_DOMAIN=${DOVECOT_DOMAIN:-"example.org"}
 DOVECOT_BASE_DN=${DOVECOT_BASE_DN:-""}
 
 # TLS
 DOVECOT_TLS=${DOVECOT_TLS:-"1"}
 DOVECOT_TLS_CA_CRT=${DOVECOT_TLS_CA_CRT:-"/etc/dovecot/certs/ca.crt"}
-DOVECOT_TLS_CA_KEY=${DOVECOT_TLS_CA_KEA:-"/etc/dovecot/certs/ca.key"}
+DOVECOT_TLS_CA_KEY=${DOVECOT_TLS_CA_KEY:-"/etc/dovecot/certs/ca.key"}
 DOVECOT_TLS_CRT=${DOVECOT_TLS_CRT:-"/etc/dovecot/certs/tls.crt"}
 DOVECOT_TLS_KEY=${DOVECOT_TLS_KEY:-"/etc/dovecot/certs/tls.key"}
 DOVECOT_TLS_DH_PARAM=${DOVECOT_TLS_DH_PARAM:-"/etc/dovecot/certs/dhparam.pem"}
@@ -23,6 +23,15 @@ DOVECOT_TLS_ENFORCE=${DOVECOT_TLS_ENFORCE:-"0"}
 DOVECOT_TLS_CIPHER_SUITE=${DOVECOT_TLS_CIPHER_SUITE:-"HIGH:-VERS-TLS-ALL:+VERS-TLS1.2:+VERS-TLS1.3:!SSLv3:!SSLv2:!ADH"}
 DOVECOT_TLS_VERIFY_CLIENT=${DOVECOT_TLS_VERIFY_CLIENT:-demand}
 
+VMAIL_UID="${VMAIL_UID:-5000}"
+
+# LDAP
+USE_LDAP=${USE_LDAP:-"0"}
+LDAP_SERVER_URL=${LDAP_SERVER_URL:-"ldap://localhost"}
+LDAP_DN_READER=${LDAP_DN_READER:-"cn=mailAccountReader,ou=Manager,dc=example,dc=org"}
+LDAP_DNPASS_READER_FILE=${LDAP_DNPASS_READER_FILE:-"/etc/dovecot-secrets/LDAP_DNPASS_READER_FILE"}
+LDAP_USE_TLS=${LDAP_USE_TLS:-"1"}
+LDAP_TLS_CA_CRT=${LDAP_TLS_CA_CRT:-"/etc/openldap/certs/openldap-ca.crt"}
 
 setup_timezone() {
     if [ -n "$TZ" ]; then
@@ -58,8 +67,57 @@ file_env() {
     unset "$fileVar"
 }
 
+setup_vmail_user() {
+    # Create the vmail user with the requested UID
+    if [ -x /usr/sbin/adduser ]; then
+        adduser -D -h /var/spool/vmail -g "Virtual Mail User" -u "${VMAIL_UID}" -s /sbin/nologin vmail
+    else
+        useradd -d /var/spool/vmail -U -c "Virtual Mail User" -u "${VMAIL_UID}" vmail
+    fi
+    if [ $? -ne 0 ]; then
+        echo "ERROR: creating of vmail user failed! Aborting."
+        exit 1
+    fi
+
+    if [ ! -d /var/spool/vmail ]; then
+        mkdir -p /var/spool/vmail
+        chown vmail:vmail /var/spool/vmail
+        chmod 775 /var/spool/vmail
+    fi
+}
+
 setup_default_config() {
-    [ "$(ls -A /etc/dovecot)" ] || cp -a /entrypoint/default-config/* /etc/dovecot/
+
+    mkdir -p "${DOVECOT_RUN_DIR}"
+
+    [ -z "$(ls -A /etc/dovecot)" ] || return
+
+    # Only continue
+    cp -a /entrypoint/default-config/* /etc/dovecot/
+
+    sed -i -e 's|#log_path =.*|log_path = /dev/stderr|g' /etc/dovecot/conf.d/10-logging.conf
+
+    if [ "${DEBUG}" = "1" ]; then
+	# Enable some debug informations in conf.d/10-logging.conf
+	sed -i -e 's|#auth_verbose =.*|auth_verbose = yes|g' /etc/dovecot/conf.d/10-logging.conf
+	sed -i -e 's|#mail_debug =.*|mail_debug = yes|g' /etc/dovecot/conf.d/10-logging.conf
+	sed -i -e 's|#verbose_ssl =.*|verbose_ssl = yes|g' /etc/dovecot/conf.d/10-logging.conf
+    fi
+}
+
+setup_ldap() {
+    [ ${USE_LDAP} = "1" ] || return
+
+    # Disable enabled auth includes and add ldap
+    sed -i -e 's|^!include\(.*\)|#!include\1|g' /etc/dovecot/conf.d/10-auth.conf
+    echo "!include auth-ldap.conf.ext" >> /etc/dovecot/conf.d/10-auth.conf
+
+    sed -i "s|#dn =.*|dn = ${LDAP_DN_READER}" /etc/dovecot/dovecot-ldap.conf.ext
+    file_env LDAP_DNPASS_READER
+    sed -i "s|#dnpass =.*|dnpass = ${LDAP_DNPASS_READER}" /etc/dovecot/dovecot-ldap.conf.ext
+    if [ "${LDAP_USE_TLS}" = "1" ]; then
+	sed -i
+
 }
 
 # if command starts with an option, prepend dovecot
@@ -68,8 +126,9 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 # Generic setup
-setup_default_config
 setup_timezone
+setup_vmail_user
+setup_default_config
 echo "Updating certificate store..."
 update-ca-certificates
 
