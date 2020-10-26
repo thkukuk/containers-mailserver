ab4f482b34f1b7afcc4bd63cdaf0b91d6c895829
Thorsten Kukuk <kukuk@suse.com>
2020-10-26 21:16:42 +0100

    Remove symlink workaround

--- postfix/config.sh
+++ postfix/config.sh
@@ -9,7 +9,3 @@ test -f /.profile && . /.profile
 # Greeting...
 #--------------------------------------
 echo "Configure image: [$kiwi_iname]..."
-
-# Fix path so that update-ca-certificates does not complain
-# [bsc#1175340]
-rm /etc/ssl/certs && ln -sf /var/lib/ca-certificates/pem /etc/ssl/certs
